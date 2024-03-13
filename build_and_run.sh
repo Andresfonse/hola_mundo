b2dc6536df7e5f70c01441f3be23a65cc4d6f1a9
Andres Eli <110643161+Andresfonse@users.noreply.github.com>
2024-03-13 00:07:13 -0500

    Update build_and_run.sh

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Verifica si el contenedor Docker ya existe y lo elimina si es así
+if [ "$(docker ps -q -f name=hola-mundo-container)" ]; then
+    docker stop hola-mundo-container
+    docker rm hola-mundo-container
+fi
+
 # Construye la imagen Docker
 docker build -t hola-mundo .
 
-# Ejecuta el contenedor a partir de la imagen construida
-docker run hola-mundo
+# Ejecuta el contenedor a partir de la imagen construida en el puerto 9000
+docker run -d -p 9000:5000 --name hola-mundo-container hola-mundo
