5b64bc260eb0b1e13f5dd2efdcc6970f3be2dd99
Andres Eli <110643161+Andresfonse@users.noreply.github.com>
2024-03-21 14:06:54 -0500

    Update hola_mundo.py

--- hola_mundo.py
+++ hola_mundo.py
@@ -6,13 +6,15 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(b"<html><head><title>Hola Mundo</title></head>")
-        self.wfile.write(b"<body><h1>¡Hola, mundo!</h1></body></html>")
+        html="""
+        <h2>hola</h2>
+        """
+        self.wfile.write(html.encode('utf-8'))
 
-def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=5656):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print(f"Servidor HTTP activo en el puerto {port}")
+def run():
+    server_address = ('', 6336)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler )
+    print(f"Servidor HTTP activo en el puerto ")
     httpd.serve_forever()
 
 if __name__ == "__main__":
