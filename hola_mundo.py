baa921fe6d270e9bb0b5fc7e6b5f2c79535c6164
Andres Eli <110643161+Andresfonse@users.noreply.github.com>
2024-03-21 01:41:26 -0500

    Update hola_mundo.py

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,18 +1,1 @@
-# hola_mundo.py
-
-def generate_html():
-    html_content = """
-    <html>
-    <head>
-        <title>Hola Mundo</title>
-    </head>
-    <body>
-        <h1>¡Hola, mundo!</h1>
-    </body>
-    </html>
-    """
-    with open('/var/www/html/hola_mundo.html', 'w') as f:
-        f.write(html_content)
-
-if __name__ == "__main__":
-    generate_html()
+holamundo
