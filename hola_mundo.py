2ae65462c3e25e2fd16677e8581b44a2906a92b4
Andres Eli <110643161+Andresfonse@users.noreply.github.com>
2024-03-21 01:46:14 -0500

    Update hola_mundo.py

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,1 +1,18 @@
-holamundo
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        SimpleHTTPRequestHandler.end_headers(self)
+
+# Cambia el directorio raíz del servidor HTTP al directorio donde se encuentra el archivo HTML
+SimpleHTTPRequestHandler.extensions_map.update({
+    '.html': 'text/html',
+})
+
+# Ejecuta el servidor HTTP en el puerto 80
+server_address = ('', 80)
+httpd = HTTPServer(server_address, MyHTTPRequestHandler)
+print('Servidor HTTP activo...')
+httpd.serve_forever()
