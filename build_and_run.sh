e3c95b860549c20509f2125ee9a24a226870c0c3
Andres Eli <110643161+Andresfonse@users.noreply.github.com>
2024-04-02 22:15:17 -0500

    Update build_and_run.sh

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Clonar el repositorio desde GitHub
-git clone https://github.com/Andresfonse/hola_mundo.git
+# Ejecutar el archivo hola_mundo.py
+python hola_mundo.py
 
-# Navegar al directorio del proyecto
-cd /
-
-# Construir la imagen Docker
-docker build -t tilinrunning .
-
-# Ejecutar el contenedor Docker
-docker run -d -p 6336:6336 tilinrunning
